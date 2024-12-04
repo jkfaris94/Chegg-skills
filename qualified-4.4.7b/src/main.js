b3e15c85fa2dc69517a742e0fe50f1ea3b3c0ce8
Johnny Faris <jkfaris@gmail.com>
2024-12-04 15:32:07 -0800

    added qualified exam for 4.4

---
+++ qualified-4.4.7b/src/main.js
@@ -0,0 +1,20 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+function index() {}
+
+function create(body) {}
+
+function show(id) {}
+
+function update(id, body) {}
+
+function destroy(id) {}
+
+module.exports = {
+  index,
+  create,
+  show,
+  update,
+  destroy,
+};
