cd66de4a2bc61157be65c9cda83e1a98f5b67fdd
Johnny Faris <jkfaris@gmail.com>
2024-12-09 11:44:52 -0800

    solved assignment. passing all tests

--- qualified-4.6.2/src/main.js
+++ qualified-4.6.2/src/main.js
@@ -4,15 +4,16 @@ const BASE_URL = "http://localhost:5000";
 function updateIfExists(id, body) {
   const url = `${BASE_URL}/constellations/${id}`;
   return axios
-    .get(url)
-    .then(() => {
-      axios.put(url, body).then(({ data }) => {
-        console.log(data);
-      });
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+  .get(url)
+  .then(() => {
+    return axios.put(url, body);
+  })
+  .then((response) => {
+    return response.data;
+  })
+  .catch((error) => {
+    return error.message;
+  });
 }
 
 module.exports = {
