2eadea4203d3b2bf0c72b77760e792f0c580e181
Johnny Faris <jkfaris@gmail.com>
2024-12-10 10:14:57 -0800

    update(constellation) passing tests

--- qualified-4.6.6/src/main.js
+++ qualified-4.6.6/src/main.js
@@ -5,7 +5,19 @@ function isValid({ id, name, meaning, quadrant, starsWithPlanets }) {
   return id && name && meaning && quadrant && starsWithPlanets;
 }
 
-function update(constellation) {}
+function update(constellation) {
+  const { id } = constellation;
+
+  const apiEndpoint = `http://localhost:5000/constellations/${id}`;
+
+  return axios.put(apiEndpoint , constellation)
+    .then(response => response)
+    .catch(() => {
+      return Promise.reject ({
+        error: `Updating constellation (id: ${id}) failed.`,
+      });
+    });
+}
 
 function bulkImport(constellations) {}
 
