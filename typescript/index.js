2e6e98c6ecfea9f6374c3e27531f65764b7037d1
Johnny Faris <jkfaris@gmail.com>
2024-12-10 14:11:13 -0800

    more lesson play

--- typescript/index.js
+++ typescript/index.js
@@ -11,3 +11,14 @@ var user1 = { name: "Alice", age: 25 };
 var user2 = { name: "Bob", age: 16 };
 processUser(user1); // Output: Alice is an adult.
 processUser(user2); // Output: Bob is a minor.
+//enum
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["Success"] = 200] = "Success";
+    StatusCode[StatusCode["NotFound"] = 404] = "NotFound";
+    StatusCode[StatusCode["Error"] = 500] = "Error";
+})(StatusCode || (StatusCode = {}));
+var response = StatusCode.Success;
+//tuple
+var book = ["The Hobbit", "J.R.R. Tolkien", 295];
+console.log(book);
--- typescript/index.ts
+++ typescript/index.ts
@@ -17,3 +17,15 @@ const user2: User = { name: "Bob", age: 16 };
 
 processUser(user1);  // Output: Alice is an adult.
 processUser(user2);  // Output: Bob is a minor.
+
+//enum
+enum StatusCode { 
+    Success = 200, 
+    NotFound = 404, 
+    Error = 500 
+  }
+  let response: StatusCode = StatusCode.Success;
+
+  //tuple
+  let book: [string, string, number] = ["The Hobbit", "J.R.R. Tolkien", 295];
+  console.log(book);
