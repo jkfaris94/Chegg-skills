6a41965cba3c1d5c2b2e15150794da6304dbeb6b
Johnny Faris <jkfaris@gmail.com>
2024-12-09 16:40:10 -0800

    added original code commented out for reference of conversion

--- qualified-4.6.5/src/main.js
+++ qualified-4.6.5/src/main.js
@@ -37,3 +37,30 @@ function fullSession(question) {
 }
 
 module.exports = { getFortune, fullSession };
+
+
+
+//original code 
+//async function getFortune(question) {
+  //try {
+  //  const response = await tell(question);
+  //  console.log(`Your question was: ${question}`);
+  //  console.log(`Your fortune is: ${response}`);
+  //} catch (err) {
+   // console.log(`There was an error: ${err}`);
+  //}
+//}
+//
+//async function fullSession(question) {
+ // try {
+   // const welcomeMessage = await welcome();
+   // console.log(welcomeMessage);
+
+   // await getFortune(question);
+
+    //const goodbyeMessage = await goodbye();
+    //console.log(goodbyeMessage);
+  //} catch (err) {
+    //console.log(`There was an error: ${err}`);
+  //}
+//}
