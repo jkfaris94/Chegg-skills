9fa2c8db4254251aa088ef18c80a662624b95499
Johnny Faris <jkfaris@gmail.com>
2024-12-11 17:05:26 -0800

    solved formatUserData function and passing all tests. using if() {} for optional inputs

--- qualified-4.7.5/src/formatUserData.ts
+++ qualified-4.7.5/src/formatUserData.ts
@@ -7,6 +7,15 @@ export interface User {
 }
 
 // Update the function below
-export function formatUserData(user) {
-  return user;
+export function formatUserData(user: User): string{
+  let formatedUser = `User ${user.id}: ${user.firstName}`;
+
+  if (user.lastName) {
+    formatedUser += ` ${user.lastName}`;
+  }
+  if (user.email) {
+    formatedUser += ` - ${user.email}`;
+  }
+
+  return formatedUser;
 }
