750eb30f6e054b0d91391e1c5982fbf7e8075801
Johnny Faris <jkfaris@gmail.com>
2025-01-14 09:23:28 -0800

    added .catch to code

--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -5,4 +5,7 @@ axios.get(url).then((response) => {
   console.log(response.status);
   console.log(response.statusText);
   console.log(response.data);
+})
+  .catch((error) => {
+    console.log(error.message);
 });
