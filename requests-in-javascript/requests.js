0a4cab16677ae0001d003fb5da516133d5bb3fba
Johnny Faris <jkfaris@gmail.com>
2025-01-14 09:28:54 -0800

    changed code to post new constellation

--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 
 const url = "http://localhost:5001/constellations";
-axios.get(url).then((response) => {
-    const result = response.data.filter((constellation) => {
-        return constellation.starsWithPlanets < 10;
-      });
-      console.log(result);
-    })
-  .catch((error) => {
-    console.log(error.message);
+axios
+.post(url, {
+    name: "Ara",
+    meaning: "Altar",
+    starsWithPlanets: 7,
+    quadrant: "SQ3"
+})
+.then((response) => {
+    console.log(response.data);
 });
