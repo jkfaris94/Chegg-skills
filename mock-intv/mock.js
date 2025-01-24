ae073c7d8799b45fbe119f962ed44a31a33aeba8
Johnny Faris <jkfaris@gmail.com>
2025-01-23 16:23:08 -0800

    finished mock.js first second test using axios and async try await.

--- mock-intv/mock.js
+++ mock-intv/mock.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
-const url = "https://api.frankfurter.app/latest?from=USD";
 
-async function getRates() {
-axios 
-    .get(url)
-    .then((response) => {
-       console.log(response.data);
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
+async function getRates(baseCurrency) {
+    const url = `https://api.frankfurter.app/latest?from=${baseCurrency}`;
+
+    try {
+    const response = await axios.get(url);
+    return response.data.rates;
+    } catch (error) {
+    console.error("Error fetching rates:", error);
 }
+}
+
+getRates("USD").then(rates => {
+    console.log("Rates:", rates);
+  });
