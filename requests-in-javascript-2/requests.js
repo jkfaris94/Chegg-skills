2d8461b0f48b6831eeb84ff150e09d32efe56db7
Johnny Faris <jkfaris@gmail.com>
2025-01-28 11:00:12 -0800

    made code using axios for get request, then made code for post requests to add a new constellation

---
+++ requests-in-javascript-2/requests.js
@@ -0,0 +1,27 @@
+const axios = require("axios");  //require axios
+
+const url = "http://localhost:5001/constellations"; //declare url 
+axios.get(url) //make get request to url 
+    .then((response) => { //response is the argument 
+        console.log(response.status); //print code of the service status
+        console.log(response.statusText); //response type of the server
+        console.log(response.data); //response body
+    })
+    .catch((error) => {
+        console.log(error.message); //print the error message if an error occurs
+    })
+
+    axios 
+        .post(url, { //post method has 2 arguments (server url, data object)
+            name: "Ara",
+            meaning: "Altar",
+            starsWithPlanets: 7,
+            quadrant: "SQ3",
+        })
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+
