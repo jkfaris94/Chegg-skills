1b88f65ac39c629c74ca463f18dcae76e8748dac
Johnny Faris <jkfaris@gmail.com>
2025-01-28 11:14:43 -0800

    commented out code for post and delete to look back at notes. TODO: bulk delete of several newly created constellations

--- requests-in-javascript-2/requests.js
+++ requests-in-javascript-2/requests.js
@@ -11,27 +11,27 @@ axios.get(url) //make get request to url
         console.log(error.message); //print the error message if an error occurs
     })
 
-    axios 
-        .post(url, { //post method has 2 arguments (server url, data object)
-            name: "Ara",
-            meaning: "Altar",
-            starsWithPlanets: 7,
-            quadrant: "SQ3",
-        })
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error.message);
-        });
+    // axios 
+    //     .post(url, { //post method has 2 arguments (server url, data object)
+    //         name: "Ara",
+    //         meaning: "Altar",
+    //         starsWithPlanets: 7,
+    //         quadrant: "SQ3",
+    //     })
+    //     .then((response) => {
+    //         console.log(response.data);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error.message);
+    //     });
 
-        const deleteURL = "http://localhost:5001/constellations/iK0hLWS" //declare new url with provided id
+    //     const deleteURL = "http://localhost:5001/constellations/iK0hLWS" //declare new url with provided id
 
-        axios
-        .delete(deleteURL) //make delete request to new url 
-        .then((response) => {
-            console.log("delete successful", response.data);
-        })
-        .catch((error) => {
-            console.log("oops", error.message);
-        });
+    //     axios
+    //     .delete(deleteURL) //make delete request to new url 
+    //     .then((response) => {
+    //         console.log("delete successful", response.data);
+    //     })
+    //     .catch((error) => {
+    //         console.log("oops", error.message);
+    //     });
