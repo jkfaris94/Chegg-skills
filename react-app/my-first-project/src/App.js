6db1dd3e8f25b90a85a69b89df245c5e27fda5a7
Johnny Faris <jkfaris@gmail.com>
2025-03-21 16:08:04 -0700

    added multiple functions

--- react-app/my-first-project/src/App.js
+++ react-app/my-first-project/src/App.js
@@ -4,8 +4,22 @@ function WelcomeBack() {
   return <p>Welcome back, valued customer!</p>;
 }
 
+function EncouragingWords() {
+  return <p>Believe it</p>;
+}
+
+function DailyTip() {
+  return <p>Highlight a whole paragraph by triple-clicking on it.</p>;
+}
+
 function App() {
-  return <WelcomeBack />;
+  return (
+    <>
+      <WelcomeBack />
+      <EncouragingWords />
+      <DailyTip />
+    </>
+  );
 }
 
 export default App;
