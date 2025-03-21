72c66b443c735da4038cfaf414b4f2aa94ed3654
Johnny Faris <jkfaris@gmail.com>
2025-03-21 12:15:35 -0700

    played with jsx and adding elements

--- react-app/my-first-project/src/App.js
+++ react-app/my-first-project/src/App.js
@@ -2,7 +2,13 @@ import "./App.css";
 
 function App() {
   const name = "Alice";
-  const element = <p>Hello, {name}!</p>;
+  const element = (
+    <>
+      <h1>Hello {name}!</h1>
+      <h2>Have a good day.</h2>
+      <p> this is further sample stuff</p>
+    </>
+  );
 
   return element;
 }
