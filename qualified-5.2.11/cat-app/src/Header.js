a2695a17ded7de39e7321242898175832153278d
Johnny Faris <jkfaris@gmail.com>
2025-03-26 13:13:22 -0700

    added code for header.ja

--- qualified-5.2.11/cat-app/src/Header.js
+++ qualified-5.2.11/cat-app/src/Header.js
@@ -5,11 +5,12 @@ import "./Header.css";
   TODO: Complete the header
 */
 
-function Header({name="Unknown Name",birthday="Unknown birthday",imageSrc="",}) {
+function Header({name="Kitty Kat",birthday="January 1",imageSrc="https://res.cloudinary.com/strive/image/upload/w_1000,h_1000,c_limit/f9c0c6f475a7d6d357f070e269501876-ple_profile_page.png",}) {
   return (
     <header>
-
-    
+      <img src={imageSrc}/>
+      <h1>{name}</h1>
+      <h2>{birthday}</h2>
     </header>
   );
 }
