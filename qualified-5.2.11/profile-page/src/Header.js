a1e29bcebcb053e637c1dddbd4598d5e0727062e
Johnny Faris <jkfaris@gmail.com>
2025-03-26 12:24:32 -0700

    added initial code to Header.js

---
+++ qualified-5.2.11/profile-page/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+/*
+  TODO: Complete the header
+*/
+
+function Header({name="Unknown Name",birthday="Unknown birthday",imageSrc="",}) {
+  return (
+    <header>
+
+    
+    </header>
+  );
+}
+
+export default Header;
