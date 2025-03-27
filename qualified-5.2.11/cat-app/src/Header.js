012d54c5de29e06eb84f56254e6880817d2755a2
Johnny Faris <jkfaris@gmail.com>
2025-03-26 19:40:48 -0700

    added some css styles to the cat photo inline

--- qualified-5.2.11/cat-app/src/Header.js
+++ qualified-5.2.11/cat-app/src/Header.js
@@ -8,7 +8,7 @@ import "./Header.css";
 function Header({name="Kitty Kat",birthday="January 1",imageSrc="https://res.cloudinary.com/strive/image/upload/w_1000,h_1000,c_limit/f9c0c6f475a7d6d357f070e269501876-ple_profile_page.png",}) {
   return (
     <header>
-      <img src={imageSrc}/>
+      <img src={imageSrc} alt="cat image" style={{width: "100px", height:"100px"}}/>
       <h1>{name}</h1>
       <h2>{birthday}</h2>
     </header>
