a3e9d4bb4211f2a1c2c2c4e78b3845a0a9cd93f7
Johnny Faris <jkfaris@gmail.com>
2025-03-31 11:59:38 -0700

    added same for the email input

--- react-app/my-first-project/src/SubscribeForm.js
+++ react-app/my-first-project/src/SubscribeForm.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 
 function SubscriberForm() {
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     const handleNameChange = (event) => setName(event.target.value);
+    const handleEmailChange = (event) => setEmail(event.target.value);
 
     console.log("Current value of name:", name);
+    console.log("Current value of email:", email);
     return (
       <form>
         <label htmlFor="name">
@@ -19,7 +22,13 @@ function SubscriberForm() {
         </label>
         <label htmlFor="email">
           Your Email:
-          <input id="email" type="email" name="email" />
+          <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleEmailChange}
+          value={email}
+        />
         </label>
       </form>
     );
