212b1121d292ccb4a7d6f781e4ca4675e9df5c48
Johnny Faris <jkfaris@gmail.com>
2025-03-31 10:49:05 -0700

    added solution for app.jsx

--- qualified-5.3.6/App.jsx
+++ qualified-5.3.6/App.jsx
@@ -3,8 +3,18 @@ import ClickTimes from "./ClickTimes";
 import TimestampsDisplay from "./TimestampsDisplay";
 
 function App() {
-  const timestamps = [1000000000000, 1000000001000];
-  return <TimestampsDisplay timestamps={timestamps} />;
+  const timestamps = [timestamp, setTimestamps] = useState([]);
+
+  const handleClick = () => {
+    setTimestamps([...timestamps, Date.now()]);
+  };
+
+  return (
+    <>
+      <ClickTimes onClick={handleClick} />
+      <TimestampsDisplay timestamps={timestamps} />
+    </>
+  );
 }
 
 export default App;
