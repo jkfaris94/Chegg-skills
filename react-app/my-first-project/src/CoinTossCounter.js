5e91366a100d1c29ea22c32f562f1fd68fb1015b
Johnny Faris <jkfaris@gmail.com>
2025-03-31 09:52:22 -0700

    added delete last button and function to delete the last clickked

--- react-app/my-first-project/src/CoinTossCounter.js
+++ react-app/my-first-project/src/CoinTossCounter.js
@@ -7,11 +7,20 @@ function CoinTossCounter() {
     const lis = results.map((result, index) => (
         <li key={`toss-${index}`}>{result === "H" ? "Heads" : "Tails"}</li>
     ));
+
+    const handleDeleteLast = () => {
+      const filteredList = results.filter(
+        (result, index) => index !== results.length - 1
+      );
+      setResults(filteredList);
+    };
+    
     return (
       <section>
         <div>
           <button onClick={() => handleClick("H")}>Heads</button>
           <button onClick={() => handleClick("T")}>Tails</button>
+          <button onClick={handleDeleteLast}>Delete Last</button>
         </div>
         <ul>{lis}</ul>
       </section>
