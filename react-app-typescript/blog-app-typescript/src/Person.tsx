efa73845151b344b8d63844b10eafa4dd36caceb
Johnny Faris <jkfaris@gmail.com>
2025-04-01 09:13:26 -0700

    filled code for person component

--- react-app-typescript/blog-app-typescript/src/Person.tsx
+++ react-app-typescript/blog-app-typescript/src/Person.tsx
@@ -1,7 +1,15 @@
-const Person = () => {
+interface PersonProps{
+    name: string;
+    age: number;
+    headshot?: string;
+}
+
+
+const Person = ({name, age, headshot}: PersonProps) => {
     return (
         <div>
-
+            <h1>{name} | {age}</h1>
+            <img src={headshot} />
         </div>
     );
 };
