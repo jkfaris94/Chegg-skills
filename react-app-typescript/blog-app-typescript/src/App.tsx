39c03ce0301a05869a9b99ccb55c6cb85ab30f9e
Johnny Faris <jkfaris@gmail.com>
2025-04-01 09:18:05 -0700

    filled out app component with person details

--- react-app-typescript/blog-app-typescript/src/App.tsx
+++ react-app-typescript/blog-app-typescript/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import Header from './Header';
+import Person from './Person';
 
 function App() {
+  const headshotUrl = "example.com/jpg"
   return (
     <div className="App">
-      
+      <Person name={"Dwayne 'The Rock' Johnson"} age={51} headshot={headshotUrl} />
     </div>
   );
 }
