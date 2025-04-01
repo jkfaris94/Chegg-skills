c9fa834918a6eeb6241a2b132e32ef9eab9b9990
Johnny Faris <jkfaris@gmail.com>
2025-03-31 22:18:55 -0700

    added radio button

--- react-app/my-first-project/src/SubscriberForm2.js
+++ react-app/my-first-project/src/SubscriberForm2.js
@@ -58,6 +58,32 @@ function SubscriberForm() {
             <option value="youtube">YouTube</option>
         </select>
         </label>
+        <br />
+        <fieldset>
+          <legend>How old are you?</legend>
+          <label htmlFor="low">
+            Under 18
+            <input
+              id="low"
+              type="radio"
+              name="age"
+              onChange={handleChange}
+              value="low"
+              checked={formData.age === "low"}
+            />
+          </label>
+          <label htmlFor="middle">
+            18 - 60
+            <input
+              id="middle"
+              type="radio"
+              name="age"
+              onChange={handleChange}
+              value="middle"
+              checked={formData.age === "middle"}
+            />
+          </label>
+        </fieldset>
         <button type="submit">Submit</button>
       </form>
     );
