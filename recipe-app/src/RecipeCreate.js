5cc3edd0450b7bf5d9eba7abadc74db40ad3fb59
Johnny Faris <jkfaris@gmail.com>
2025-04-02 23:01:09 -0700

    added some bootstrap to the create form

--- recipe-app/src/RecipeCreate.js
+++ recipe-app/src/RecipeCreate.js
@@ -24,67 +24,66 @@ function RecipeCreate({ createRecipe }) {
   };
 
   return (
-    <section>
+    <section className="mt-3">
       <h2>Add a new Recipe</h2>
-      <form name="create" onSubmit={handleSubmit}>
-      <label htmlFor="name">   
-        <input 
-          id="name"
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={handleChange}
-          value={formData.name}
-        />
-      </label>
-      <br />
-      <label htmlFor="cuisine">
-        <input 
-          id="name"
-          type="text"
-          name="cuisine"
-          placeholder="cuisine"
-          onChange={handleChange}
-          value={formData.cuisine}
-        />
-      </label>
-      <br />
-      <label htmlFor="photo">
-        <input 
-          id="photo"
-          type="url"
-          name="photo"
-          placeholder="URL"
-          onChange={handleChange}
-          value={formData.photo}
-        />
-      </label>
-      <br />
-      <label htmlFor="ingredients">
-        <textarea 
-          id="ingredients"
-          type="text"
-          name="ingredients"
-          placeholder="Ingredients"
-          onChange={handleChange}
-          value={formData.ingredients}
-        >
-        </textarea>
-      </label>
-      <br />
-      <label htmlFor="preparation">
-        <textarea 
-          id="preparation"
-          type="text"
-          name="preparation"
-          placeholder="Preparation"
-          onChange={handleChange}
-          value={formData.preparation}
-        >
-        </textarea>
-      </label>
-      <br />
-      <button type="submit">Create</button>
+      <form name="create" onSubmit={handleSubmit}  className="mt-4">
+      <div className="row g-3 mb-3">
+        <label htmlFor="name" className="col-md-4">   
+          <input 
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+          />
+        </label>
+        <label htmlFor="cuisine" className="col-md-4">
+          <input 
+            id="name"
+            type="text"
+            name="cuisine"
+            placeholder="cuisine"
+            onChange={handleChange}
+            value={formData.cuisine}
+          />
+        </label>
+        <label htmlFor="photo" className="col-md-4">
+          <input 
+            id="photo"
+            type="url"
+            name="photo"
+            placeholder="URL"
+            onChange={handleChange}
+            value={formData.photo}
+          />
+        </label>
+      </div>
+      <div className="row g-3 align-items-stretch">
+        <label htmlFor="ingredients" className="col-md-4">
+          <textarea 
+            id="ingredients"
+            type="text"
+            name="ingredients"
+            placeholder="Ingredients"
+            onChange={handleChange}
+            value={formData.ingredients}
+          >
+          </textarea>
+        </label>
+        <label htmlFor="preparation" className="col-md-4">
+          <textarea 
+            id="preparation"
+            type="text"
+            name="preparation"
+            placeholder="Preparation"
+            onChange={handleChange}
+            value={formData.preparation}
+          >
+          </textarea>
+        </label>
+        <button type="submit" className="col-md-4">Create</button>
+      </div>
     </form>
     </section>
   );
