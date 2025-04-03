397460b82691c65cb980e63fa9238d5766510d5e
Johnny Faris <jkfaris@gmail.com>
2025-04-02 20:47:32 -0700

    added create recipe function

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -7,13 +7,22 @@ import recipeData from "./data";
 function App() {
   const [recipes, setRecipes] = useState(recipeData);
 
+  const createRecipe = (newRecipe) => 
+    setRecipes((currentRecipes) => [
+      newRecipe,
+      ...currentRecipes,
+    ])
+
   return (
     <main className="container">
       <header className="bg-primary bg-gradient text-white rounded-bottom">
         <h1 className="text-center py-5 display-5">Delicious Food Recipes</h1>
       </header>
-      <RecipeList recipes={recipes}/>
-      <RecipeCreate />
+      <RecipeList 
+      recipes={recipes}
+      deleteRecipe={deleteRecipe}
+      />
+      <RecipeCreate createRecipe={createRecipe}/>
     </main>
   );
 }
