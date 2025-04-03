7cc8b37b7078078cbf4b7623f09004a5035f802c
Johnny Faris <jkfaris@gmail.com>
2025-04-02 20:05:12 -0700

    set form html layout

--- recipe-app/src/RecipeCreate.js
+++ recipe-app/src/RecipeCreate.js
@@ -19,7 +19,7 @@ function RecipeCreate() {
 
   const handleSubmit = (event) => {
     event.preventDefault(); 
-    console.log("Submitted:", FormData);
+    console.log("Submitted:", formData);
     setFormData({ ...initialFormState });
   };
 
@@ -28,7 +28,64 @@ function RecipeCreate() {
   // TODO: Add the required submit and change handlers.
   
   return (
-    <form></form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">   
+        <input 
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+      </label>
+      <br />
+      <label htmlFor="cuisine">
+        <input 
+          id="name"
+          type="text"
+          name="cuisine"
+          placeholder="cuisine"
+          onChange={handleChange}
+          value={formData.cuisine}
+        />
+      </label>
+      <br />
+      <label htmlFor="photo">
+        <input 
+          id="photo"
+          type="url"
+          name="photo"
+          placeholder="URL"
+          onChange={handleChange}
+          value={formData.photo}
+        />
+      </label>
+      <br />
+      <label htmlFor="ingredients">
+        <input 
+          id="ingredients"
+          type="text"
+          name="ingredients"
+          placeholder="Ingredients"
+          onChange={handleChange}
+          value={formData.ingredients}
+        />
+      </label>
+      <br />
+      <label htmlFor="preparation">
+        <input 
+          id="preparation"
+          type="text"
+          name="preparation"
+          placeholder="Preparation"
+          onChange={handleChange}
+          value={formData.preparation}
+        />
+      </label>
+      <br />
+      <button type="submit">Create</button>
+    </form>
   );
 }
 
