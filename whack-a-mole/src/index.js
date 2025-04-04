1fee4a12ea1303c06f739653d5e9aa2dc095fc4c
Johnny Faris <jkfaris@gmail.com>
2025-04-04 11:45:35 -0700

    used provided code for gameOver()

--- whack-a-mole/src/index.js
+++ whack-a-mole/src/index.js
@@ -105,12 +105,12 @@ function chooseHole(holes) {
 */
 function gameOver() {
   // TODO: Write your code here
-  // if time > 0:
-  //   timeoutId = showUp()
-  //   return timeoutId
-  // else
-  //   gameStopped = stopGame()
-  //   return gameStopped
+  if time > 0:
+    timeoutId = showUp()
+    return timeoutId
+  else
+    gameStopped = stopGame()
+    return gameStopped
 }
 
 /**
