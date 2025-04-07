617350de127bc517b65ca6ea522a88bac42b5ada
Johnny Faris <jkfaris@gmail.com>
2025-04-07 12:16:46 -0700

    completed solution, all tests are passing

--- qualified-5.5.2/React_and_APIs_with_hooks_Effect_hook_React_18_1/src/App.js
+++ qualified-5.5.2/React_and_APIs_with_hooks_Effect_hook_React_18_1/src/App.js
@@ -5,6 +5,18 @@ function App() {
 
   // Load data from https://jsonplaceholder.typicode.com/todos?userId=2
 
+  useEffect(() => { async function loadToDos() {
+    try { 
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos?userId=2");
+      const data = await response.json();
+      setToDos(data);
+    } catch (error) {
+      console.error("Failed to fetch todos:", error);
+    }
+  };
+
+  loadToDos();
+  }, []);
   return (
     <div className="App">
       <h1>To Do List </h1>
