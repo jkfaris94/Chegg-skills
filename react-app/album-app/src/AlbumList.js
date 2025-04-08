e608992a74b9f5082b4addc5a42e0be84030e912
Johnny Faris <jkfaris@gmail.com>
2025-04-07 22:31:00 -0700

    tests are all passing

--- react-app/album-app/src/AlbumList.js
+++ react-app/album-app/src/AlbumList.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function AlbumList({ user = {} }) {
-  return <p>Please click on a user name to the left</p>;
+  const [albums, setAlbums] = useState([]);
+
+  
+  useEffect(() => {
+    if (!user.id) return;
+
+    const abortController = new AbortController(); //create new AbortController
+    //code want to run
+    async function loadAlbums() {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/albums?userId=${user.id}`,
+          { signal : abortController.signal } //pass abort
+        );
+        const userAlbums = await response.json();
+        setAlbums(userAlbums);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Aborted", user.id);
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    loadAlbums();
+    //return function
+
+    return () => {
+      console.log("cleanup", user.id);
+      abortController.abort();
+    };
+  }, [user.id]); //dependency array
+
+  if (!user.id) {
+    return <p>Please click on a user name to the left</p>;
+  }
+
+  return (
+    <div>
+      <h2>Albums for {user.name}</h2>
+      <ul>
+        {albums.map((album) => (
+          <li key={album.id}>{album.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 
 export default AlbumList;
--- react-app/album-app/src/App.js
+++ react-app/album-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import AlbumList from "./AlbumList";
@@ -9,6 +9,24 @@ function App() {
   const [currentUser, setCurrentUser] = useState({});
 
   // Load data from https://jsonplaceholder.typicode.com/albums?userId=${user.id}
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    
+    async function loadUsers() {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users",
+          { signal: abortController.signal }
+        );
+        const usersFromAPI = await response.json();
+        setUsers(usersFromAPI);
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      }
+    }
+
+    loadUsers();
+  }, []);
 
   return (
     <div className="App">
