64448559c5432e7a8ea5f28b5332b32173ed5081
Johnny Faris <jkfaris@gmail.com>
2025-04-08 16:12:37 -0700

    completed lesson walkthru for using react-router-dom

--- react-app/router-exercise-app/src/App.js
+++ react-app/router-exercise-app/src/App.js
@@ -1,11 +1,44 @@
 import React from "react"
 import './App.css';
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router,
+   Link,
+   Routes,
+   Route,
+   useLocation,
+  } from "react-router-dom";
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
+
+function NoMatch() {
+  const location = useLocation();
+
+  return (
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Routes>{/* cYou will define our routes here using <Route> */}</Routes>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">About</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
