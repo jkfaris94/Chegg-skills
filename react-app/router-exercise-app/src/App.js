488a0378dc827131cdccaff057644b8bbe5dbca3
Johnny Faris <jkfaris@gmail.com>
2025-04-08 15:56:20 -0700

    initial files for router-exercise-app lesson

---
+++ react-app/router-exercise-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>{/* cYou will define our routes here using <Route> */}</Routes>
+    </Router>
+  );
+}
+
+export default App;
