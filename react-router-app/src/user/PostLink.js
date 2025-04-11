5dcda2f1b89c903b669badabeab2e8283f242948
Johnny Faris <jkfaris@gmail.com>
2025-04-11 16:53:17 -0700

    changed a tag to be link that goes to the specific post route using the post id.

--- react-router-app/src/user/PostLink.js
+++ react-router-app/src/user/PostLink.js
@@ -1,11 +1,14 @@
+import { useParams, Link } from "react-router-dom";
+
 /*
-  TODO: Change the below to be a link that goes to the specific post route using the post id. Hint: you can use the `useParams()` hook from "react-router-dom" to get the current userId.
+  xxTODO: Change the below to be a link that goes to the specific post route using the post id. Hint: you can use the `useParams()` hook from "react-router-dom" to get the current userId.
 */
 
 function PostLink ({ post }) {
+  const { userID } = useParams();
   return (
     <li className="list-group-item text-truncate">
-      <a>{post.title}</a>
+      <Link>{`/users/${userID}/posts/${post.id}`}</Link>
     </li>
   );
 };
