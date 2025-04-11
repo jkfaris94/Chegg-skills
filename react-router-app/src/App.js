1abe19f493ef52f82b7c6922c9cdc0b168002c8e
Johnny Faris <jkfaris@gmail.com>
2025-04-11 16:16:32 -0700

    created HomePage function for homepage to match with example and structered Routes

--- react-router-app/src/App.js
+++ react-router-app/src/App.js
@@ -1,14 +1,43 @@
 import Header from "./common/Header";
 import User from "./user/User";
 import CardList from "./home/CardList";
+import { Route, Routes } from "react-router-dom";
+import Card from "./home/Card";
+import PostsNav from "./user/PostsNav";
+import PostLink from "./user/PostLink";
+import UserProfile from "./user/UserProfile";
+import PostList from "./user/PostList";
+import Post from "./user/Post";
 
-function App() {
+function HomePage() {
   return (
     <>
       <Header />
-      <User />
-      <CardList /> 
+      <CardList />
     </>
+  );
+}
+
+
+function App() {
+  return (
+      <div>
+        <Routes>
+          <Route path="header" element={<Header />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="user" element={<User />}>
+            <Route path="posts-nav" element={<PostsNav/>} />
+            <Route path="post-list" element ={<PostList />}>
+              <Route path="post-link" element={<PostLink />} />
+              <Route path="Post" element={<Post />} />
+            </Route>
+            <Route path="user-profile" element={< UserProfile/>} />
+          </Route>
+          <Route path="card-list" element={<CardList />}>
+            <Route path="card" element={<Card />} />
+          </Route>
+        </Routes>
+      </div>
   );
 }
 
