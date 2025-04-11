e9b97d96e5b9f286830bdba515b6f2094513e216
Johnny Faris <jkfaris@gmail.com>
2025-04-11 15:24:25 -0700

    added Link to PostNav

--- react-router-app/src/user/PostsNav.js
+++ react-router-app/src/user/PostsNav.js
@@ -1,11 +1,12 @@
 // TODO: Change the link below to go back to the home page.
+import { Link } from "react-router-dom";
 
 function PostsNav () {
   return (
     <nav aria-label="breadcrumb">
       <ol className="breadcrumb">
         <li className="breadcrumb-item">
-          <a className="btn btn-link">Go Home</a>
+          <Link to="/" className="btn btn-link">Go Home</Link>
         </li>
       </ol>
     </nav>
