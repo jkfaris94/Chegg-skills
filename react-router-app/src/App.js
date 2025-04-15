90fbe102c45773625868abb3dc4dbdd02e6044dc
Johnny Faris <jkfaris@gmail.com>
2025-04-14 21:41:48 -0700

    updated postlist to add user.posts wherever posts is

--- react-router-app/src/App.js
+++ react-router-app/src/App.js
@@ -1,36 +1,31 @@
-// import Header from "./common/Header";
+import Header from "./common/Header";
 import User from "./user/User";
 import CardList from "./home/CardList";
 import { Route, Routes } from "react-router-dom";
-// import Card from "./home/Card";
+import Card from "./home/Card";
 import UserProfile from "./user/UserProfile";
 import PostList from "./user/PostList";
 import Post from "./user/Post";
 import NotFound from "./common/NotFound";
-import MainLayout from "./home/MainLayout";
+
 // import PostsNav from "./user/PostsNav";
 
 function App() {
   return (
       <div>
+        < Header />
         <Routes>
-          <Route path="/" element={<MainLayout />} >
-          <Route index element={<CardList />} />
+          
+          <Route path="/" element={<CardList />} />
 
           <Route path="users/:userId" element={<User />}>
-            {/* <Route index element={<PostsNav />} /> */}
             <Route index element={< UserProfile />} />
             <Route path="posts" element ={<PostList />} >
               <Route path=":postId" element={<Post />} />
               </Route>
           </Route>
 
-          {/* <Route path="users" element={<CardList />}>
-            <Route path="card" element={<Card />} />
-          </Route> */}
-
           <Route path="*" element={<NotFound />} />
-          </Route>
         </Routes>
       </div>
   );
--- react-router-app/src/user/PostList.js
+++ react-router-app/src/user/PostList.js
@@ -1,7 +1,6 @@
 import PostLink from "./PostLink";
 import NoPostSelectedMessage from "./NoPostSelectedMessage";
 import { Outlet, useParams, useLocation, useOutletContext  } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 /*
   TODO: xxUpdate the below so that the components show on the appropriate route.
@@ -18,23 +17,15 @@ import { useEffect, useState } from "react";
 function PostList () {
   const { userId } = useParams();
   const location = useLocation();
-  const [posts, setPosts] = useState(null);
   const { user } = useOutletContext();
   
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
-      .then((response) => response.json())
-      .then((data) => setPosts(data));
-  }, [userId]);
-
   const postListRoute = location.pathname === `/users/${userId}/posts`;
 
-  if (!posts || !Array.isArray(posts)) {
+  if (!user || !user.posts || !Array.isArray(user.posts)) {
     return <p>Loading posts...</p>; 
   }
 
-  const postLinks = posts.map((post) => (
+  const postLinks = user.posts.map((post) => (
     <PostLink key={post.id} userId={userId} post={post} />
   ));
 
