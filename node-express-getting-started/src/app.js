d74222cbf8c72c51e444a0ab3e43d5068cd939a8
Johnny Faris <jkfaris@gmail.com>
2025-04-24 21:53:45 -0700

    added explanation notes

--- node-express-getting-started/src/app.js
+++ node-express-getting-started/src/app.js
@@ -1,5 +1,9 @@
+// The Express package is required and is assigned to a variable.
 const express = require("express");
 
+// The Express package exports a function. 
+// When you call that function, you get a new Express application and assign it to a variable.
 const app = express();
 
+// The Express application is exported to be used in the server.js file.
 module.exports = app;
--- node-express-getting-started/src/server.js
+++ node-express-getting-started/src/server.js
@@ -1,5 +1,18 @@
+// With destructuring and default arguments, set the variable PORT
+// to be equal to whatever value is found inside of process.env or default to 5000.
 const { PORT = 5000 } = ProcessingInstruction.env;
 
+// The exported Express application is required.
 const app = require("./app");
 
+// This function will run when the server successfully starts.
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+
+// The listen() method enables the application to accept incoming network requests.
+// When you invoke this method on your Express application instance, it instructs the underlying Node.js HTTP 
+// server to start listening for requests on a specified port.
+// This action effectively initializes the server, making your application accessible over the network. 
+// The listen() method contains two arguments: a port number and a function.
+// The PORT variable defines where your server is running, and the listener() function will get called as 
+// soon as the server has successfully started.
+app.listen(PORT, listener);
