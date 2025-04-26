94d8048172533d43d6438871ae625397e89901b5
Johnny Faris <jkfaris@gmail.com>
2025-04-25 21:44:59 -0700

    lesson on using middlware and morgan

--- node-express-getting-started/src/app.js
+++ node-express-getting-started/src/app.js
@@ -1,9 +1,22 @@
 // The Express package is required and is assigned to a variable.
 const express = require("express");
+//require Morgan
+const morgan = require("morgan");
 
 // The Express package exports a function. 
 // When you call that function, you get a new Express application and assign it to a variable.
 const app = express();
+
+const sayHello = (req, res, next) => {
+    // Middleware function body
+    res.send("Hello!");
+  };
+  
+app.use(morgan("dev"));
+app.use(sayHello);
+
+
+
 
 // The Express application is exported to be used in the server.js file.
 module.exports = app;
