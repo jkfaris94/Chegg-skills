67a2d0b373967d026b1175b6cd7ff69a273b8656
Johnny Faris <jkfaris@gmail.com>
2025-05-12 15:43:35 -0700

    added sqipt examples

--- notes/postgresql-scripts.sql
+++ notes/postgresql-scripts.sql
@@ -29,3 +29,79 @@ WHERE body IS NULL;
 SELECT *
 FROM articles
 WHERE body IS NOT DISTINCT FROM NULL;
+
+/*Satisfying two conditions with AND*/
+SELECT product_name, aisle, department
+FROM products
+WHERE department = 'bakery'
+AND aisle = 'bakery desserts';
+
+/*Satisfying two conditions with OR*/
+SELECT product_name, aisle, department
+FROM products
+WHERE department = 'bakery'
+OR aisle = 'bakery desserts';
+
+/* Stacking conditions with AND and comparison operators */
+SELECT product_name, price
+FROM products
+WHERE department = 'beverages'
+AND aisle = 'tea';
+
+SELECT product_name, price
+FROM products
+WHERE department = 'beverages'
+AND aisle = 'tea'
+AND price < 15
+AND price > 10;
+
+
+/*  Add the DISTINCT operator to the query to remove duplicates from the results. */
+SELECT DISTINCT aisle
+FROM products;
+
+SELECT DISTINCT aisle
+FROM products
+WHERE department = 'beverages';
+
+/* you can count the number of rows that have distinct aisles by using the count() aggregate function and DISTINCT: */
+SELECT count(DISTINCT aisle)
+FROM products;
+
+/*LIKE*/
+SELECT product_name, price
+FROM products
+WHERE department = 'beverages'
+AND aisle = 'tea'
+AND price < 15
+AND price > 10
+AND product_name LIKE '%Raspberry%';
+
+/*ORDER BY*/
+SELECT product_name, price
+FROM products
+WHERE department = 'pets'
+ORDER BY price;
+
+/*DESC */
+SELECT product_name, price
+FROM products
+WHERE department = 'pets'
+ORDER BY price DESC;
+
+/* GROUP BY */
+SELECT department, count(*)
+FROM products
+GROUP BY department;
+
+                            /* Aggregate functions */
+ /* sum() */
+ SELECT department, sum(price)
+FROM products
+GROUP BY department;
+
+/* min(), max(), and avg() */
+SELECT department, count(*), sum(price), min(price), max(price), avg(price)
+FROM products
+GROUP BY department
+ORDER BY department;
