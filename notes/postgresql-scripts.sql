d38b9feecdefd0b580823b696d2c4d0f3c5d4083
Johnny Faris <jkfaris@gmail.com>
2025-05-16 15:48:45 -0700

    added more example scripts

--- notes/postgresql-scripts.sql
+++ notes/postgresql-scripts.sql
@@ -168,4 +168,15 @@ FROM
   departments
   JOIN employees
   ON departments.manager = employees.employee_id;
-  
+
+  -- Create table aliases
+  SELECT
+  d.department_id,
+  d.department_name,
+  e.first_name,
+  e.last_name,
+  e.salary
+FROM
+  departments d -- Create alias `d` for the `departments` table
+  JOIN employees e -- Create alias `e` for the `employees` table
+  ON d.manager = e.employee_id;
