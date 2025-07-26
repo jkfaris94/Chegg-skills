a0f76a65b0141dcf9b821c2b58faba479991592d
Johnny <jkfaris@gmail.com>
2025-07-26 14:38:02 -0700

    added solution

--- making-web-requests-intv/Mock1/src/solution.js
+++ making-web-requests-intv/Mock1/src/solution.js
@@ -2,6 +2,21 @@ const axios = require("axios");
 //Write your functions here
 
 
+async function getRates(CUR) {
+   const URL = `https://api.frankfurter.app/latest?from=${CUR}`;
+
+   try {
+        const response = await axios.get(URL);
+        return response.data.rates;
+   } catch (error) {
+        console.error("Error fetching data:", error);
+        throw new Error("Failed to fetch exchange rates");
+   }
+}
+
+getRates("USD")
+   .then(rates => console.log(rates))
+   .catch(error => console.error(error));
 
 //Do not modify code below this line
 module.exports = {getRates};
