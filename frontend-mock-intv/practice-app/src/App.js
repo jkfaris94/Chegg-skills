c422c502eacd283236192be4f96b1743b3996ec5
Johnny <jkfaris@gmail.com>
2025-07-31 14:55:02 -0700

    solved practice problem 2, changed api from quotes to posts since the quote API was not working

--- frontend-mock-intv/practice-app/src/App.js
+++ frontend-mock-intv/practice-app/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import GreetingCard from './practice1';
+import FetchQuote from './practice2';
 
 function App() {
   return (
     <>
     <GreetingCard name="Johnny" />
     <GreetingCard />
+    <FetchQuote />
     </>
   );
 }
--- frontend-mock-intv/practice-app/src/practice2.js
+++ frontend-mock-intv/practice-app/src/practice2.js
@@ -18,6 +18,51 @@
 
 // Button triggers a new fetch
 
-function FetchUser() {
-    
+
+// ***altered example to a different API and used math.random to generate random index to use post over quote
+import { useState, useEffect } from "react";
+
+
+export default function FetchQuote() {
+        const [post, setPost] = useState({ title: "", body: "" });
+        const [loading, setLoading] = useState(true);
+
+        async function loadPost() {
+            try {
+            setLoading(true);
+
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+            if (!response.ok) throw new Error("Fetch failed");
+
+            const data = await response.json();
+            const random = data[Math.floor(Math.random() * data.length)];
+
+            setPost({ title: random.title, body: random.body });
+            } catch (err) {
+            console.error("Fetch error:", err);
+
+            } finally {
+            setLoading(false);
+            }
+        }
+  
+
+  useEffect(() => {
+    loadPost();
+  }, []);
+
+  return (
+    <>
+      <h2>Amazing Post</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p>-{post.title}</p>
+          <p>"{post.body}"</p>
+        </>
+      )}
+      <button onClick={loadPost}>Get New Post</button>
+    </>
+  );
 }
