dc06e1b2541e01fb617e2858b808f97884321781
Johnny <jkfaris@gmail.com>
2025-08-06 22:17:28 -0700

    added 2 application level middleware examples

--- 3-backend-development/node-express/src/app.js
+++ 3-backend-development/node-express/src/app.js
@@ -2,4 +2,17 @@ const express = require("express") //The Express package is required and is assi
 
 const app = express(); //The Express package exports a function. When you call that function, you get a new Express application and assign it to a variable.
 
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+};
+
+const logging = (req, res, next) => {
+    console.log("a request is being made");
+
+    next();
+}
+
+app.use(logging);
+app.use(sayHello);
+
 module.exports = app; // The Express application is exported to be used in the server.js file.
