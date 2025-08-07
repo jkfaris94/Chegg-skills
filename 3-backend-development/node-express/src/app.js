d7389b8e043843da2ea7f4f375aad5eca9c01544
Johnny <jkfaris@gmail.com>
2025-08-06 22:27:37 -0700

    installed and implemented morgan for better information logging to terminal

--- 3-backend-development/node-express/src/app.js
+++ 3-backend-development/node-express/src/app.js
@@ -1,18 +1,24 @@
 const express = require("express") //The Express package is required and is assigned to a variable.
+const morgan = require("morgan");
 
 const app = express(); //The Express package exports a function. When you call that function, you get a new Express application and assign it to a variable.
 
+//application level middlewares:
+
+//Responsive middleware
 const sayHello = (req, res, next) => {
     res.send("Hello!");
 };
 
-const logging = (req, res, next) => {
-    console.log("a request is being made");
+// //Nonresponsive middleware
+// const logging = (req, res, next) => {
+//     console.log("a request is being made");
 
-    next();
-}
+//     next();
+// }
 
-app.use(logging);
+// app.use(logging);
+app.use(morgan("dev"));
 app.use(sayHello);
 
 module.exports = app; // The Express application is exported to be used in the server.js file.
