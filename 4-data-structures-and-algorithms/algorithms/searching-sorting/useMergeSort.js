7058cc452839d0e766273df2234acd772b30395a
Johnny <jkfaris@gmail.com>
2025-08-12 22:57:19 -0700

    added notes for merge sort

--- 4-data-structures-and-algorithms/algorithms/searching-sorting/useMergeSort.js
+++ 4-data-structures-and-algorithms/algorithms/searching-sorting/useMergeSort.js
@@ -8,3 +8,12 @@ function compare(left, right) {
 }
 
 console.log(sort(compare, elements));
+
+
+
+//Merge sort algorithm 
+// ** one of the most efficent **
+// A divide-and-conquer algorithm that continuously splits arrays in half until every element 
+// is alone in its own array, then merges each subarray in order
+//----- worst case - O(n log n) ----- avg case - O(n log n) ----- best case - O(n log n)
+// ALL CASES - O(n log n)
