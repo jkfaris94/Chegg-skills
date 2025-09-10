f7473c020fa82ae84c6276a20e782e9d8dd31613
Johnny <jkfaris@gmail.com>
2025-09-10 11:36:12 -0700

    added solution for minimumAndMaximum

--- 4-data-structures-and-algorithms/qualified-8.6.2/Data_structures-_Arrays/src/minimumAndMaximum.js
+++ 4-data-structures-and-algorithms/qualified-8.6.2/Data_structures-_Arrays/src/minimumAndMaximum.js
@@ -1,11 +1,43 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
-function minimumAndMaximumSort(numbers) {}
+function minimumAndMaximumSort(numbers) {
+  if (numbers.length === 0) return [];
+
+  let min = numbers[0];
+  let max = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let isMin = true;
+    let isMax = true;
+
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[j] < numbers[i]) isMin = false;
+      if (numbers[j] > numbers[i]) isMax = false;
+    }
+
+    if (isMin) min = numbers[i];
+    if (isMax) max = numbers[i];
+  }
+
+  return [min, max];
+}
 
 /**
  * Implement an algorithm that uses iteration to find the minimum and maximum
  */
-function minimumAndMaximumIterate(numbers) {}
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length === 0) return [];
+
+  let min = numbers[0];
+  let max = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) min = numbers[i];
+    if (numbers[i] > max) max = numbers[i];
+  }
+
+  return [min, max];
+}
 
 module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
